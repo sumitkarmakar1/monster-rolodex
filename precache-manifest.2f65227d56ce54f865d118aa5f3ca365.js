08e24da67f697f7c11a8007237926200e217a813
sumitkarmakar1 <sam33kumar@gmail.com>
2019-11-25 14:06:17 +0530

    Updates

---
+++ precache-manifest.2f65227d56ce54f865d118aa5f3ca365.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f5a3cac190f74ab84ec2df8432ad66",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "7a25b836e3a998e385dd",
+    "url": "/monster-rolodex/static/css/main.9afcbdd7.chunk.css"
+  },
+  {
+    "revision": "2ea260805c2d8eeb767a",
+    "url": "/monster-rolodex/static/js/2.bce2a7e5.chunk.js"
+  },
+  {
+    "revision": "7a25b836e3a998e385dd",
+    "url": "/monster-rolodex/static/js/main.17759cce.chunk.js"
+  },
+  {
+    "revision": "aff08a4d83f58743dcb3",
+    "url": "/monster-rolodex/static/js/runtime-main.400ce9bc.js"
+  }
+]);
